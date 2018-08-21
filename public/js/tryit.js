81ac14622fe99e6e1a2670638e1a185a57927ce9
Jesse Owens <jesse@jessecowens.com>
2018-08-21 07:25:03 -0600

    Rename $ to $demoServer

--- public/js/tryit.js
+++ public/js/tryit.js
@@ -1,6 +1,5 @@
-function demoServer(){}
 jQuery(document).ready(
-  function($) {
+  function($demoServerdemoServer) {
     var tryit_terms_hash = "";
     var tryit_console = "";
     var tryit_server = "tryit.whatdoyouneedthatfordude.com";
@@ -85,7 +84,7 @@ jQuery(document).ready(
                 screenKeys: false
             });
 
-            $('#tryit_console_reconnect').css("display", "none");
+            $demoServerdemoServer('#tryit_console_reconnect').css("display", "none");
             term.open(document.getElementById("tryit_console"))
 
             term.on('data', function(data) {
@@ -98,7 +97,7 @@ jQuery(document).ready(
 
             sock.onclose = function(msg) {
                 term.destroy();
-                $('#tryit_console_reconnect').css("display", "inherit");
+                $demoServer('#tryit_console_reconnect').css("display", "inherit");
             };
         };
     }
@@ -135,7 +134,7 @@ jQuery(document).ready(
         return cell;
     }
 
-    $('.tryit_goback').click(function() {
+    $demoServer('.tryit_goback').click(function() {
         window.location.href = original_url;
     });
 
@@ -154,12 +153,12 @@ jQuery(document).ready(
     }
     setupPre()
 
-    $('.tryit_run').click(function() {
+    $demoServer('.tryit_run').click(function() {
         if (!term || !sock) {
             return;
         }
 
-        data = $(this).text()
+        data = $demoServer(this).text()
         sock.send(data);
         sock.send("\n");
     });
@@ -167,94 +166,94 @@ jQuery(document).ready(
     tryit_console = getUrlParameter("id");
 
     if (tryit_console == "") {
-        $.ajax({
+        $demoServer.ajax({
             url: "https://"+tryit_server+"/1.0",
             success: function(data) {
                 if (data.server_console_only == true) {
-                    $('#tryit_ssh_row').css("display", "none");
-                    $('#tryit_lxd_row').css("display", "none");
+                    $demoServer('#tryit_ssh_row').css("display", "none");
+                    $demoServer('#tryit_lxd_row').css("display", "none");
                 }
 
                 if (data.server_status == 1) {
-                    $('#tryit_maintenance_message').css("display", "inherit");
-                    $('#tryit_status_panel').css("display", "inherit");
-                    $('#tryit_status_panel').addClass('panel-warning');
-                    $('#tryit_status_panel').removeClass('panel-success');
+                    $demoServer('#tryit_maintenance_message').css("display", "inherit");
+                    $demoServer('#tryit_status_panel').css("display", "inherit");
+                    $demoServer('#tryit_status_panel').addClass('panel-warning');
+                    $demoServer('#tryit_status_panel').removeClass('panel-success');
                     return
                 }
 
-                $('#tryit_protocol').text(data.client_protocol);
-                $('#tryit_address').text(data.client_address);
-                $('#tryit_count').text(data.containers_count);
-                $('#tryit_max').text(data.containers_max);
-                $('#tryit_online_message').css("display", "inherit");
-                $('#tryit_status_panel').css("display", "inherit");
+                $demoServer('#tryit_protocol').text(data.client_protocol);
+                $demoServer('#tryit_address').text(data.client_address);
+                $demoServer('#tryit_count').text(data.containers_count);
+                $demoServer('#tryit_max').text(data.containers_max);
+                $demoServer('#tryit_online_message').css("display", "inherit");
+                $demoServer('#tryit_status_panel').css("display", "inherit");
 
-                $.ajax({
+                $demoServer.ajax({
                     url: "https://"+tryit_server+"/1.0/terms"
                 }).then(function(data) {
                     tryit = data;
-                    $('#tryit_terms').html(data.terms);
+                    $demoServer('#tryit_terms').html(data.terms);
                     tryit_terms_hash = data.hash;
-                    $('#tryit_terms_panel').css("display", "inherit");
-                    $('#tryit_start_panel').css("display", "inherit");
+                    $demoServer('#tryit_terms_panel').css("display", "inherit");
+                    $demoServer('#tryit_start_panel').css("display", "inherit");
                 });
 
             },
             error: function(data) {
-                $('#tryit_unreachable_message').css("display", "inherit");
-                $('#tryit_status_panel').css("display", "inherit");
-                $('#tryit_status_panel').addClass('panel-danger');
-                $('#tryit_status_panel').removeClass('panel-success');
+                $demoServer('#tryit_unreachable_message').css("display", "inherit");
+                $demoServer('#tryit_status_panel').css("display", "inherit");
+                $demoServer('#tryit_status_panel').addClass('panel-danger');
+                $demoServer('#tryit_status_panel').removeClass('panel-success');
                 return
             }
         });
     } else {
-        $.ajax({
+        $demoServer.ajax({
             url: "https://"+tryit_server+"/1.0/info?id="+tryit_console,
             success: function(data) {
                 if (data.status && data.status != 0) {
-                    $('#tryit_start_panel').css("display", "none");
-                    $('#tryit_error_missing').css("display", "inherit");
-                    $('#tryit_error_panel_access').css("display", "inherit");
-                    $('#tryit_error_panel').css("display", "inherit");
+                    $demoServer('#tryit_start_panel').css("display", "none");
+                    $demoServer('#tryit_error_missing').css("display", "inherit");
+                    $demoServer('#tryit_error_panel_access').css("display", "inherit");
+                    $demoServer('#tryit_error_panel').css("display", "inherit");
                     return
                 }
 
-                $('.tryit_container_id').text(data.id);
-                $('.tryit_container_ip').text(data.ip);
-                $('.tryit_container_fqdn').text(data.fqdn);
-                $('.tryit_container_username').text(data.username);
-                $('.tryit_container_password').text(data.password);
+                $demoServer('.tryit_container_id').text(data.id);
+                $demoServer('.tryit_container_ip').text(data.ip);
+                $demoServer('.tryit_container_fqdn').text(data.fqdn);
+                $demoServer('.tryit_container_username').text(data.username);
+                $demoServer('.tryit_container_password').text(data.password);
 
                 initializeClock('tryit_clock', data.expiry);
 
-                $('#tryit_status_panel').css("display", "none");
-                $('#tryit_start_panel').css("display", "none");
-                $('#tryit_info_panel').css("display", "inherit");
-                $('#tryit_console_panel').css("display", "inherit");
-                $('#tryit_examples_panel').css("display", "inherit");
+                $demoServer('#tryit_status_panel').css("display", "none");
+                $demoServer('#tryit_start_panel').css("display", "none");
+                $demoServer('#tryit_info_panel').css("display", "inherit");
+                $demoServer('#tryit_console_panel').css("display", "inherit");
+                $demoServer('#tryit_examples_panel').css("display", "inherit");
 
                 tryit_console = data.id;
                 window.history.pushState("", "", "?id="+tryit_console);
                 setupConsole(tryit_console);
             },
             error: function(data) {
-                $('#tryit_start_panel').css("display", "none");
-                $('#tryit_error_missing').css("display", "inherit");
-                $('#tryit_error_panel_access').css("display", "inherit");
-                $('#tryit_error_panel').css("display", "inherit");
+                $demoServer('#tryit_start_panel').css("display", "none");
+                $demoServer('#tryit_error_missing').css("display", "inherit");
+                $demoServer('#tryit_error_panel_access').css("display", "inherit");
+                $demoServer('#tryit_error_panel').css("display", "inherit");
                 return
             }
         });
     }
 
-    $('#tryit_accept').click(function() {
-        $('#tryit_terms_panel').css("display", "none");
-        $('#tryit_accept').css("display", "none");
-        $('#tryit_progress').css("display", "inherit");
+    $demoServer('#tryit_accept').click(function() {
+        $demoServer('#tryit_terms_panel').css("display", "none");
+        $demoServer('#tryit_accept').css("display", "none");
+        $demoServer('#tryit_progress').css("display", "inherit");
 
-        $.ajax({
+        $demoServer.ajax({
             url: "https://"+tryit_server+"/1.0/start?terms="+tryit_terms_hash
         }).then(function(data) {
             if (data.status && data.status != 0) {
@@ -263,36 +262,36 @@ jQuery(document).ready(
                     return
                 }
 
-                $('#tryit_start_panel').css("display", "none");
+                $demoServer('#tryit_start_panel').css("display", "none");
                 if (data.status == 2) {
-                    $('#tryit_error_full').css("display", "inherit");
+                    $demoServer('#tryit_error_full').css("display", "inherit");
                 }
                 else if (data.status == 3) {
-                    $('#tryit_error_quota').css("display", "inherit");
+                    $demoServer('#tryit_error_quota').css("display", "inherit");
                 }
                 else if (data.status == 4) {
-                    $('#tryit_error_banned').css("display", "inherit");
+                    $demoServer('#tryit_error_banned').css("display", "inherit");
                 }
                 else if (data.status == 5) {
-                    $('#tryit_error_unknown').css("display", "inherit");
+                    $demoServer('#tryit_error_unknown').css("display", "inherit");
                 }
-                $('#tryit_error_panel_create').css("display", "inherit");
-                $('#tryit_error_panel').css("display", "inherit");
+                $demoServer('#tryit_error_panel_create').css("display", "inherit");
+                $demoServer('#tryit_error_panel').css("display", "inherit");
                 return
             }
 
-            $('.tryit_container_console').text(data.id);
-            $('.tryit_container_ip').text(data.ip);
-            $('.tryit_container_fqdn').text(data.fqdn);
-            $('.tryit_container_username').text(data.username);
-            $('.tryit_container_password').text(data.password);
+            $demoServer('.tryit_container_console').text(data.id);
+            $demoServer('.tryit_container_ip').text(data.ip);
+            $demoServer('.tryit_container_fqdn').text(data.fqdn);
+            $demoServer('.tryit_container_username').text(data.username);
+            $demoServer('.tryit_container_password').text(data.password);
             initializeClock('tryit_clock', data.expiry);
 
-            $('#tryit_status_panel').css("display", "none");
-            $('#tryit_start_panel').css("display", "none");
-            $('#tryit_info_panel').css("display", "inherit");
-            $('#tryit_console_panel').css("display", "inherit");
-            $('#tryit_examples_panel').css("display", "inherit");
+            $demoServer('#tryit_status_panel').css("display", "none");
+            $demoServer('#tryit_start_panel').css("display", "none");
+            $demoServer('#tryit_info_panel').css("display", "inherit");
+            $demoServer('#tryit_console_panel').css("display", "inherit");
+            $demoServer('#tryit_examples_panel').css("display", "inherit");
 
             tryit_console = data.id;
             window.history.pushState("", "", "?id="+tryit_console);
@@ -300,24 +299,24 @@ jQuery(document).ready(
         });
     });
 
-    $('#tryit_console_reconnect').click(function() {
+    $demoServer('#tryit_console_reconnect').click(function() {
         setupConsole(tryit_console);
     });
 
-    $('#tryit_intro').on('shown.bs.collapse', function (e) {
-        var offset = $('.panel.panel-default > .panel-collapse.in').offset();
+    $demoServer('#tryit_intro').on('shown.bs.collapse', function (e) {
+        var offset = $demoServer('.panel.panel-default > .panel-collapse.in').offset();
         if(offset) {
-            $('html,body').animate({
-                scrollTop: $('.panel-collapse.in').siblings('.panel-heading').offset().top - 50
+            $demoServer('html,body').animate({
+                scrollTop: $demoServer('.panel-collapse.in').siblings('.panel-heading').offset().top - 50
             }, 500);
         }
     });
 
-    $('.tabNext').click(function(){
-        $('.nav-tabs > .active').next('li').find('a').trigger('click');
+    $demoServer('.tabNext').click(function(){
+        $demoServer('.nav-tabs > .active').next('li').find('a').trigger('click');
     });
 
-    $('.tabPrevious').click(function(){
-        $('.nav-tabs > .active').prev('li').find('a').trigger('click');
+    $demoServer('.tabPrevious').click(function(){
+        $demoServer('.nav-tabs > .active').prev('li').find('a').trigger('click');
     });
 }).apply(demoServer);
