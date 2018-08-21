2d0ebfc879a9d355dfb8de3b8ae37a2fe1c07ebe
Jesse Owens <jesse@jessecowens.com>
2018-08-20 18:18:07 -0600

    Updated login link to register link

--- public/class-jco-demo-server-public.php
+++ public/class-jco-demo-server-public.php
@@ -111,7 +111,7 @@ class Jco_Demo_Server_Public {
 	}
 	public function handle_jco_demo_server_shortcode() {
 			if ( !is_user_logged_in() ) {
-					return '<div><p>You must be logged in to use the interactive terminal. <a href="' . wp_login_url() . '">Log In or Register</a></p></div>';
+					return '<div><p>You must be logged in to use the interactive terminal. <a href="' . wp_registration_url() . '">Log In or Register</a></p></div>';
 			}
 			$demo_html = '
 			<div class="panel panel-success" id="tryit_status_panel" style="display:none">
