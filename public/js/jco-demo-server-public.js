b2189548cfc1450d5355cef5ba36816a0bee174c
Jesse Owens <jesse@jessecowens.com>
2018-08-22 19:28:17 -0600

    adding some more explicit declarations?

--- public/js/jco-demo-server-public.js
+++ public/js/jco-demo-server-public.js
@@ -9,9 +9,9 @@
 
 	function getUrlParameter(sParam) {
 			var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-					sURLVariables = sPageURL.split('&'),
-					sParameterName,
-					i;
+					var sURLVariables = sPageURL.split('&'),
+					var sParameterName,
+					var i;
 
 			for (i = 0; i < sURLVariables.length; i++) {
 					sParameterName = sURLVariables[i].split('=');
